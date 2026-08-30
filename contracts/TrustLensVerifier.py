ced2051b8b35c884a9e4c521328e45c6357b02b3
YousufAziz1 <yousufaziz1234@gmail.com>
2026-08-30 15:08:38 +0530

    fix(contract): updated GenLayer Depends magic comment header

--- contracts/TrustLensVerifier.py
+++ contracts/TrustLensVerifier.py
@@ -1,5 +1,4 @@
-# v0.2.16
-# { "Depends": "py-genlayer:1jb45aa8ynh2a0c0xn3b7qqh8sm5q03hw4p7jqmwsfhh8jpz00h6" }
+# { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
 
 from genlayer import *
 import json
